c49b902f6372a1a31d47a9bc93e0b28390bc31ba
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-21 16:17:38 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,48 +5,35 @@ require 'pry'
 class InteractiveRecord
 
   def self.table_name
-    #table_name code
+      self.to_s.downcase.pluralize
+    end
+
+    def self.column_names
+      def self.column_names
+          DB[:conn].results_as_hash = true
+
+          sql = "pragma table_info('#{table_name}')"
+
+          table_info = DB[:conn].execute(sql)
+          column_names = []
+          table_info.each do |row|
+            column_names << row["name"]
+          end
+          column_names.compact
+        end
+
+        self.column_names.each do |col_name|
+          attr_accessor col_name.to_sym
+        end
+
+  def initialize(options={})
+  options.each do |property, value|
+    self.send("#{property}=", value)
   end
 
-  def self.column_names
-    #column_names code
-  end
 
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
+    def initialize(options = {})
 
-#   def self.table_name
-#       self.to_s.downcase.pluralize
-#     end
-#
-#     def self.column_names
-#       DB[:conn].results_as_hash = true
-#
-#       sql = "pragma table_info('#{table_name}')"
-#
-#       table_info = DB[:conn].execute(sql)
-#       column_names = []
-#       table_info.each do |row|
-#         column_names << row["name"]
-#       end
-#       column_names.compact
-#       # binding.pry
-#     end
-#
-#     self.column_names.each do |col_name|
-#     test = attr_accessor col_name.to_sym
-#     binding.pry
-#   end
-#
-#   def initialize(options={})
-#   options.each do |property, value|
-#     self.send("#{property}=", value)
-#   end
-#
-#
-#     def initialize(options = {})
-#
-#     end
-#
-# end
+    end
+
+end
