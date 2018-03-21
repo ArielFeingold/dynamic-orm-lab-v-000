b0f35a613b08865a16a347e86eb2d7a291ecc958
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-21 17:26:36 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
 
   def self.find_by(hash)
     array = hash.to_a.flatten
-    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{array[0].to_s} = #{array[1]}"
     DB[:conn].execute(sql, array[0].to_s, array[1])
   end
 
