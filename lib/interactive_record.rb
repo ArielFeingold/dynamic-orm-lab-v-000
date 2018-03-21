ceb2dfb4830193a19bdeaa0097ab599ae6011305
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-21 17:41:31 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(attribute)
+  def self.find_by(attribute = {})
     binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0]} = #{attribute.values[0]}"
     DB[:conn].execute(sql)
