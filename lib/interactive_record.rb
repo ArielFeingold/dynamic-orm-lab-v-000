3c7aac4550cabe1b1fc02b9295567f80c73f44a2
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-21 16:11:19 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,7 +19,7 @@ class InteractiveRecord
         column_names << row["name"]
       end
       column_names.compact
-      binding.pry
+      # binding.pry
     end
 
     self.column_names.each do |col_name|
